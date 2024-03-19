4353dcb6d2e557e05a69be854fba84a1bd8f9f4e
Gavin Taylor <gavintaylor.ai@gmail.com>
2024-03-18 22:45:42 -0500

    Updated css of site. Changed js layout.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -8,16 +8,22 @@ window.addEventListener("DOMContentLoaded", () => {
         
         // loop through each project
         for (let i = 0; i < projectsObjArray.length; ++i) {
-            const fragment = document.createDocumentFragment();
-            const paragraphElement = document.createElement('p');
-            const spaceParaElement = document.createElement('p');
+            const containerElement = document.createElement('div');
+            const divElement = document.createElement('div');
             const brElement = document.createElement('br');
             const aElement = document.createElement('a');
+            const descriptionElement = document.createElement('p');
+            const spaceElement = document.createElement('p');
             
             // array of spans for each projects skills
             let spanElementsArray = [];
+
+            containerElement.setAttribute("class", "project-container");
+            divElement.setAttribute("class", "project-div");
+            spaceElement.innerText = ' - ';
+            descriptionElement.innerText = projectsObjArray[i].description;
             
-            // a element for each project
+            // <a> element for each project
             aElement.setAttribute("class", "project-link");
             aElement.setAttribute("id", projectsObjArray[i].id);
             aElement.setAttribute("onclick", `window.open('${projectsObjArray[i].url}', '_blank')`);
@@ -27,40 +33,47 @@ window.addEventListener("DOMContentLoaded", () => {
             for (let j = 0; j < projectsObjArray[i].skills.length; ++j) {
                 const spanElement = document.createElement('span');
                 spanElement.setAttribute("class", projectsObjArray[i].skills[j].skillType);
-                spanElement.innerHTML = projectsObjArray[i].skills[j].skillName;
+
+                // conditional to add comma or newline to end of skillName
+                if (j === projectsObjArray[i].skills.length -1) {
+                    spanElement.innerHTML = projectsObjArray[i].skills[j].skillName + '\n';
+                }
+                else {
+                    spanElement.innerHTML = projectsObjArray[i].skills[j].skillName + ', ';
+                }
 
                 spanElementsArray.push(spanElement);
             }
 
-            // add project description
-            paragraphElement.innerText = projectsObjArray[i].description;
-            spaceParaElement.innerText = ' - ';
+            // Append each part to the div and container
+            divElement.append(aElement);  
+            divElement.append(spaceElement);
+            // append spans to div
+            for (let i = 0; i < spanElementsArray.length; ++i) {
+                divElement.append(spanElementsArray[i]);
+            }
+            descriptionElement.append(projectsObjArray[i].description);
+            containerElement.append(divElement);
+            containerElement.append(descriptionElement);
             
-            // fragment the elements to create a tree
-            let proj = fragment
-                .appendChild(aElement)
-                // .appendChild(spaceParaElement)
-                for (let i = 0; i < spanElementsArray.length; ++i) {
-                    fragment.appendChild(spanElementsArray[i])
-                }
-                fragment.appendChild(paragraphElement);
+            document.getElementsByClassName('projects')[0].appendChild(containerElement);
+            document.getElementsByClassName('projects')[0].appendChild(brElement);
             
-            // document.getElementsByClassName('projects')[0].appendChild(paragraphElement);
-            document.getElementsByClassName('projects')[0].appendChild(fragment);
-            document.getElementsByClassName('projects')[0].appendChild(brElement);
-            document.getElementsByClassName('projects')[0].appendChild(brElement);
+
 
             // if the project has a non null value in array object, create and append the image
             if (projectsObjArray[i].img !== null) {
                 const websiteImg = document.createElement('img');
+                const brElement1 = document.createElement('br');
+
                 websiteImg.src = projectsObjArray[i].img;
                 websiteImg.setAttribute("class", "website-img");
                 websiteImg.setAttribute("alt", projectsObjArray.imgAlt)
                 
                 document.getElementsByClassName('projects')[0].appendChild(websiteImg);
+                document.getElementsByClassName('projects')[0].appendChild(brElement1);
             }
 
-            document.getElementsByClassName('projects')[0].appendChild(brElement);
             document.getElementsByClassName('projects')[0].appendChild(brElement);
         }
     }
